0d2cb803b5a3caf322afac7851207971bc12b2ab
Arthur Nascimento <arthurnasoliveira@gmail.com>
2026-04-10 16:59:41 -0300

    ajuste build

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: import.meta.env.API_URL || 'http://localhost:8080'
 })
 
 // Interceptor para adicionar o Token em cada requisição
